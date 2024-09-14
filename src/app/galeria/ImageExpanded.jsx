2b728b54f025bebb6f03d4477c83d0168df36e3d
LorKamiT <lorkamimau@gmail.com>
2024-09-14 05:36:35 -0600

    a?

--- src/app/galeria/ImageExpanded.jsx
+++ src/app/galeria/ImageExpanded.jsx
@@ -6,7 +6,7 @@ const ImageExpanded = ({ image, onClose }) => {
   return (
     <div className="z-50 fixed top-0 left-0 w-full h-svh bg-black/80 flex justify-center items-center">
       <div className="fixed flex justify-center items-center w-full h-full max-w-screen-[95svw] max-h-screen-[95svh]">
-        <Image src={image.src} alt={image.name} className="absolute object-contain w-full h-full" width={1920} height={1080} />
+        <Image src={image.src} alt={image.name} className="absolute object-contain w-full h-full" width={500} height={500} />
         <button onClick={onClose} className="absolute top-4 right-4 text-white text-2xl">
           <MdClose />
         </button>
