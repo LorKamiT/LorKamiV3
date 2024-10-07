56da70f09194d13979546593a0a944745637bc1b
LorKamiT <lorkamimau@gmail.com>
2024-10-07 05:02:07 -0600

    up

--- src/app/guias/MenuGuias.tsx
+++ src/app/guias/MenuGuias.tsx
@@ -45,7 +45,7 @@ export default function MenuGuias() {
                   key={subtitle.sectionId}
                   className={`border-l border-[#e8eaee] transition-colors dark:border-[#1d2126] dark:text-[#b6bec9] ${
                     activeSection === subtitle.sectionId
-                      ? "border-[#3399ff] bg-[#ebf5ff] text-[#006bd6] transition-colors hover:translate-x-0.5 hover:bg-[#d6ebff] dark:border-[#3399ff] dark:bg-[#1d2126] dark:text-[#66b3ff] dark:hover:bg-[#14181b] dark:hover:text-white"
+                      ? "border-[#3382d1] bg-[#ebf5ff] text-[#006bd6] transition-colors hover:translate-x-0.5 hover:bg-[#d6ebff] dark:border-[#3399ff] dark:bg-[#1d2126] dark:text-[#66b3ff] dark:hover:bg-[#14181b] dark:hover:text-white"
                       : "hover:translate-x-0.5 hover:border-[#1d2126] hover:bg-[#f6f7f8] hover:font-semibold hover:text-[#090b0b] dark:hover:bg-[#14181b] dark:hover:text-white"
                   }`}
                 >
