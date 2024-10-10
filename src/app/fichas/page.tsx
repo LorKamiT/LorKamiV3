92f29ac587a65ed1415cccae3877aa319750e11a
LorKamiT <lorkamimau@gmail.com>
2024-10-10 02:39:55 -0600

    dell inecessary file

--- src/app/fichas/page.tsx
+++ src/app/fichas/page.tsx
@@ -2,6 +2,12 @@
 
 import FichaPersonajes from "../../components/FichaPersonajes";
 
+import { Personajes } from "";
+
+interface CharacterCardProps {
+  personaje: Personajes;
+}
+
 export default function Page() {
   return <FichaPersonajes />;
 }
